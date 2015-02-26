346d99b6f370d1b4761a68bfc1c8686220ee41a0
David Dollar <david@dollar.io>
2015-02-25 22:27:08 -0500

    new features and tests

--- logger.go
+++ logger.go
@@ -2,33 +2,61 @@ package logger
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"time"
 )
 
 type Logger struct {
 	namespace string
+	now       func() string
+	started   time.Time
+	writer    io.Writer
 }
 
 func New(ns string) *Logger {
-	return &Logger{namespace: ns}
+	return &Logger{namespace: ns, writer: os.Stdout}
 }
 
 func (l *Logger) At(at string) *Logger {
 	return l.Namespace("at=%s", at)
 }
 
-func (l *Logger) Log(format string, args ...interface{}) {
-	fmt.Printf("date=%q %s %s\n", time.Now().UTC().Format("2006-01-02T15:04:05.000000"), l.namespace, fmt.Sprintf(format, args...))
-}
-
-func (l *Logger) Namespace(format string, args ...interface{}) *Logger {
-	return &Logger{namespace: fmt.Sprintf("%s %s", l.namespace, fmt.Sprintf(format, args...))}
-}
-
 func (l *Logger) Error(err error) {
 	l.Log("state=error error=%q", err)
 }
 
+func (l *Logger) Log(format string, args ...interface{}) {
+	if l.started.IsZero() {
+		l.writer.Write([]byte(fmt.Sprintf("date=%q %s %s\n", l.now(), l.namespace, fmt.Sprintf(format, args...))))
+	} else {
+		elapsed := float64(time.Now().Sub(l.started).Nanoseconds()) / 1000
+		l.writer.Write([]byte(fmt.Sprintf("date=%q %s %s elapsed=%0.3f\n", l.now(), l.namespace, fmt.Sprintf(format, args...), elapsed)))
+	}
+}
+
+func (l *Logger) Namespace(format string, args ...interface{}) *Logger {
+	return &Logger{
+		namespace: fmt.Sprintf("%s %s", l.namespace, fmt.Sprintf(format, args...)),
+		now:       l.now,
+		started:   l.started,
+		writer:    l.writer,
+	}
+}
+
+func (l *Logger) Start() *Logger {
+	return &Logger{
+		namespace: l.namespace,
+		now:       l.now,
+		started:   time.Now(),
+		writer:    l.writer,
+	}
+}
+
 func (l *Logger) Success(format string, args ...interface{}) {
 	l.Log("state=success %s", fmt.Sprintf(format, args...))
+}
+
+func now() string {
+	return time.Now().UTC().Format("2006-01-02T15:04:05.000000")
 }
---
+++ logger_test.go
@@ -0,0 +1,86 @@
+package logger
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type BufferWriter struct {
+	buffer string
+}
+
+func (bw *BufferWriter) Buffer() string {
+	return bw.buffer
+}
+
+func (bw *BufferWriter) Clear() {
+	bw.buffer = ""
+}
+
+func (bw *BufferWriter) Line() string {
+	return strings.Split(bw.Buffer(), "\n")[0]
+}
+
+func (bw *BufferWriter) Write(data []byte) (int, error) {
+	bw.buffer += string(data)
+	return len(data), nil
+}
+
+var writer = &BufferWriter{}
+
+var log = &Logger{
+	namespace: "ns=test",
+	writer:    writer,
+	now: func() string {
+		return ""
+	},
+}
+
+func TestAt(t *testing.T) {
+	writer.Clear()
+	log.At("target").Log("foo=bar")
+	assertEquals(t, writer.Line(), `date="" ns=test at=target foo=bar`)
+}
+
+func TestError(t *testing.T) {
+	writer.Clear()
+	log.Error(fmt.Errorf("broken"))
+	assertEquals(t, writer.Line(), `date="" ns=test state=error error="broken"`)
+}
+
+func TestLog(t *testing.T) {
+	writer.Clear()
+	log.Log("string=%q int=%d float=%0.2f", "foo", 42, 3.14159)
+	assertEquals(t, writer.Line(), `date="" ns=test string="foo" int=42 float=3.14`)
+}
+
+func TestNamespace(t *testing.T) {
+	writer.Clear()
+	log.Namespace("foo=bar").Namespace("baz=qux").Log("fred=barney")
+	assertEquals(t, writer.Line(), `date="" ns=test foo=bar baz=qux fred=barney`)
+}
+
+func TestStart(t *testing.T) {
+	writer.Clear()
+	log.Start().Success("num=%d", 42)
+	assertContains(t, writer.Line(), "elapsed=")
+}
+
+func TestSuccess(t *testing.T) {
+	writer.Clear()
+	log.Success("num=%d", 42)
+	assertEquals(t, writer.Line(), `date="" ns=test state=success num=42`)
+}
+
+func assertContains(t *testing.T, got, search string) {
+	if strings.Index(got, search) == -1 {
+		t.Errorf("\n   expected: %q\n to contain: %q", got, search)
+	}
+}
+
+func assertEquals(t *testing.T, got, expected interface{}) {
+	if expected != got {
+		t.Errorf("\nexpected: %q\n     got: %q", expected, got)
+	}
+}
