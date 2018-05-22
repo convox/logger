e39179955b5204aa8288932fa09fa17e5525c55f
David Dollar <david@dollar.io>
2018-05-22 17:44:07 -0400

    better error handling

--- logger.go
+++ logger.go
@@ -10,10 +10,11 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"runtime"
 	"runtime/debug"
 	"strings"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 func init() {
@@ -62,14 +63,27 @@ func (l *Logger) Prepend(format string, args ...interface{}) *Logger {
 		writer:    l.writer,
 	}
 }
+
+type tracer interface {
+	StackTrace() errors.StackTrace
+}
+
 func (l *Logger) Error(err error) error {
-	if _, file, line, ok := runtime.Caller(1); ok {
+	if err == nil {
+		return nil
+	}
+
+	message := strings.Replace(err.Error(), "\n", " ", -1)
+
+	switch t := err.(type) {
+	case tracer:
+		file := strings.Split(fmt.Sprintf("%+s", t.StackTrace()[0]), "\n\t")[1]
 		if f, err := stripGoPath(file); err == nil {
 			file = f
 		}
-		l.Logf("state=error error=%q location=%q", strings.Replace(err.Error(), "\n", " ", -1), fmt.Sprintf("%s:%d", file, line))
-	} else {
-		l.Logf("state=error error=%q", err)
+		l.Logf("error=%q location=%q", message, fmt.Sprintf("%s:%d", file, t.StackTrace()[0]))
+	case error:
+		l.Logf("error=%q", message)
 	}
 
 	return err
